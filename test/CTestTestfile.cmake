b92fb80de9789e007d52b74ca8f6d838a4eaade6
charles <the_bracher@yahoo.com>
2014-08-25 21:36:33 -0700

    removing some cmake kruft

--- test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/charles/cql-interface/cql-interface/test
-# Build directory: /home/charles/cql-interface/cql-interface/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(cql_interface_tests "cql_interface_tests" "--logger_config" "/home/charles/cql-interface/cql-interface/test/../../config/log_info")
